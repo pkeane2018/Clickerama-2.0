f1c638232398682eb54a54ce1be394e610817e37
Patrick Keane <pkeane92@yahoo.com>
2019-02-13 17:33:17 -0600

    Publish react app to Github pages

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./style.css";
+
+function Navbar() {
+
+    return (
+        <div>
+            <nav className="navbar navbar-dark bg-primary">
+                <ul className="navbar-nav">
+                    <li className="navbar-brand">
+                        Clickerama
+        </li>
+                </ul>
+            </nav>
+        </div>
+
+    )
+
+};
+
+export default Navbar;
