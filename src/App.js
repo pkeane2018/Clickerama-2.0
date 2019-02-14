7f5b3f1b544280d3adae03fde41150356a8ac254
Patrick Keane <pkeane92@yahoo.com>
2019-02-13 18:54:52 -0600

    set router for serving page hopefully

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import './App.css';
-import Navbar from "./components/Navbar";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Home from "./components/Home.js";
 
 function App() {
    
-      return <Navbar />;
+      return (
+        <Router>
+          <Route exact path="/" component={Home}/>
+        </Router>
+
+      )
   }
 
 
---
+++ src/components/Home.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Navbar from "./components/Nabvar";
+
+function Home() {
+
+        return <Navbar />;
+    
+};
+
+export default Home;
