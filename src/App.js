6fa572867e5234aef1f5649b62088f1cc4d6f308
Patrick Keane <pkeane92@yahoo.com>
2019-02-13 20:38:24 -0600

    changed back to how it was before setting up server

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import Home from "./components/Home.js";
+import Navbar from "./components/Navbar";
 
 function App() {
    
       return (
-        <Router>
-          <Route exact path="/" component={Home}/>
-        </Router>
+       <Navbar />
 
       )
   }
